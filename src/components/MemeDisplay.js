a9e509bb156853021647127055890fa66bb37a6d
ifedeejoy <ifedeejoy@gmail.com>
2023-01-06 21:16:50 +0100

    added save button, to allow users download image

--- src/components/MemeDisplay.js
+++ src/components/MemeDisplay.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { saveAs } from 'file-saver';
 import Styles from '../styles/MemeDisplay.module.css';
 const MemeDisplay = (props) => {
+    const downloadImage = () => {
+        saveAs(props.memeUrl, Date.now() + ".png")
+    }
     return (
         <div>
             {props.memeUrl &&
                 <div className={Styles.memeDisplay}>
                     <img className={Styles.img} src={props.memeUrl || 'https://via.placeholder.com/512x512'} alt="AI Generated meme" />
+                    <button className={Styles.btn} onClick={downloadImage}>Download Image</button>
                 </div>
             }
         </div>
